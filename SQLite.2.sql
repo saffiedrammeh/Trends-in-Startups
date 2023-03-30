9ef9c54c3943dc1dc68956c35d75c88d387a2875
saffiedrammeh <129075648+saffiedrammeh@users.noreply.github.com>
2023-03-30 16:05:58 -0400

    Add files via upload

---
+++ SQLite.2.sql
@@ -0,0 +1,27 @@
+SELECT *
+FROM startups;
+
+
+SELECT name, category, ROUND (AVG (valuation),2)
+FROM startups
+GROUP BY category
+ORDER BY category;
+
+SELECT category, ROUND(AVG(valuation), 2)
+FROM startups
+GROUP BY category
+ORDER BY  valuation DESC;
+
+SELECT COUNT(name), category
+FROM startups
+GROUP BY category;
+
+SELECT category, COUNT(*)
+FROM startups
+GROUP BY category
+HAVING COUNT (category) > 3;*/
+
+ SELECT location, AVG(employees)
+ FROM startups
+ GROUP BY location
+ HAVING AVG (employees) > 500;
